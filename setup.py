c9e61a487365f86d0a81a7cc7e289d5ca65c9116
Jérôme Deuchnord <jerome@deuchnord.fr>
2021-11-10 18:50:16 +0100

    Bump v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="aurornis",
-    version="v1.0.0",
+    version="v1.0.1",
     packages=["aurornis"],
     scripts=[],
     url="https://github.com/Deuchnord/Aurornis",
